fef89bf6b4a195756eb05f764b3fd748a69d1a72
Anneyou <anne_13243188185@163.com>
2019-11-18 14:34:58 +0800

    feat: modifying the app.vue and main.js

--- guixi_vue/src/router/index.js
+++ guixi_vue/src/router/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Building from '@/components/Building'
+import Unit from '@/components/Unit'
+import Company from '@/components/Company'
 
 Vue.use(Router)
+
+Vue.prototype.baseUrl = 'http://localhost:3000/'
+
+Vue.prototype.baseCompany = '成都高新区桂溪街道办事处'
+
+Vue.prototype.baseDescription = '人口服务管理社会化信息采集点'
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      path: '/home',
+      component: Home
+    },
+    {
+      name: 'Building',
+      path: '/building',
+      component: Building
+    },
+    {
+      name: 'Unit',
+      path: '/unit',
+      component: Unit
+    },
+    {
+      name: 'Company',
+      path: '/company',
+      component: Company
     }
   ]
 })
