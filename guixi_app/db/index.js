b4e0d5403299e6e029ebe4345cbf713dc8d0cae9
Anneyou <anne_13243188185@163.com>
2019-11-19 15:04:59 +0800

    fix: modifying the home_form_id and company_form_id

--- guixi_app/db/index.js
+++ guixi_app/db/index.js
@@ -2,8 +2,8 @@ let db = {
   conStr: "postgres://postgres:@116.62.79.136:5433/postgres",
   namespace_id: '1',
   prefix: 'test',
-  home_form_id: '507',
-  company_form_id:'508',
+  home_form_id: '515',
+  company_form_id:'516',
   enterprise_form_id:'510'
 };
 module.exports = db
