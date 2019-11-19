947a7f79b1127f2f3a07888edf50fdf79c46ea08
Anneyou <anne_13243188185@163.com>
2019-11-19 15:00:59 +0800

    fix: modifying the host

--- guixi_app/app.js
+++ guixi_app/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //设置跨域访问
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Cache-Control","no-cache");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
